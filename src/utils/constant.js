d00d3f7abd718d0570ee80393603d67c0d4e1350
mrperfectVip <kk7012234@gmail.com>
2026-02-19 01:00:41 +0530

    fix: admin logout API

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -231,6 +231,6 @@ export const courseCategories = [
 ];
 
 
-// export const BASE_URL = "https://lms-backend-amm2.onrender.com/api/v1"
-export const BASE_URL = "http://localhost:3000/api/v1"
+export const BASE_URL = "https://lms-backend-amm2.onrender.com/api/v1"
+// export const BASE_URL = "http://localhost:3000/api/v1"
 
